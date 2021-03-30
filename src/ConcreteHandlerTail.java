0eada086260f818bb19062be8661cd85556136f3
bncliu <55820467+bncliu@users.noreply.github.com>
2021-03-29 18:57:54 -0700

    Add files via upload

---
+++ src/ConcreteHandlerTail.java
@@ -0,0 +1,15 @@
+import java.util.Observable;
+
+public class ConcreteHandlerTail extends Observable implements Handler{
+	public ConcreteHandlerTail() {
+		System.out.println("I am a ConcreteHandlerTail, I am just born");
+	}
+	
+	public void handleRequest(Gift gift) {
+		System.out.println("I am a ConcreteHandlerTail, this is my handleRequest method working");
+		System.out.println("- This gift has wrong size: ");
+		gift.print();
+		setChanged();
+		notifyObserver(); 
+	}
+}
---
+++ src/ConcreteHandlerWithBox.java
@@ -0,0 +1,29 @@
+import javax.swing.Box;
+
+public class ConcreteHandlerWithBox implements Handler{
+	
+	private Handler successor; 
+	
+	public ConcreteHandlerWithBox() {
+		System.out.println("I am a ConcreteHandlerBox, I am just born"); 
+	}
+	
+	public void setSuccessor(Handler handler) {successor = handler;}
+	
+	@Override 
+	public void handleRequest(Gift gift) {
+		System.out.println("I am a ConcreteHandlerBox, this is my handleRequest method working");
+		System.out.println("- I put gifts in Boxes");
+		if(gift.getSize() > 10) {
+			Box box = new Box(); 
+			box.add(gift); 
+			System.out.println("- I create a new Boc for a gift as follows:"); 
+			box.print();
+		}
+		else {
+			System.out.println("- This gift is NOT for a Box, passing it to the next Handler ..."); 
+			successor.handleRequest(gift);
+		}
+	}
+
+}
